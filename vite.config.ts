72d1b4d23cef02aa06574a9b33e7809047db058a
Alica Wziontek <Alica.Wziontek@db-n.com>
2025-05-18 15:56:55 +0200

    feat(build): add vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(), 
+    vercel()
+  ],
 });
